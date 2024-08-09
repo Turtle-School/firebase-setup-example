0403ddcf36eee15302a951174f5ccd44e7840638
cassina <bernardocassina@gmail.com>
2024-08-09 09:56:19 -0600

    feat: setup firestore and create test doc, add firestore to serve script

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,24 @@
 import express, {Request, Response} from "express";
 import {onRequest} from "firebase-functions/v2/https";
 import {initializeApp} from "firebase-admin/app";
+import {getFirestore, DocumentReference} from "firebase-admin/firestore";
 
-const app = initializeApp();
+initializeApp();
+const firestore = getFirestore();
 
 const apiRouter = express();
-apiRouter.use("/", (req: Request, res: Response) => {
-  console.log(`APP: ${JSON.stringify(app)}`);
 
-  res.status(200).json(app.name);
+apiRouter.use("/", async (req: Request, res: Response) => {
+  // console.log(`APP: ${JSON.stringify(app)}`);
+
+  const docRef: DocumentReference = firestore.collection("users").doc("goku");
+
+  const operation = await docRef.set({
+    "name": "Son Goku",
+    "born": "1980",
+  });
+
+  res.status(200).json(JSON.stringify(operation));
 });
 
 exports.api = onRequest(apiRouter);
